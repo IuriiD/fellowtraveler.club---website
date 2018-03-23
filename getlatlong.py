920ff9fdccbbd99cf8452e05a858bb0c6fc3d7e5
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-03-23 09:20:26 +0000

    Passing geodata from GMaps Autocomplete jQuery script to Flask - done

--- getlatlong.py
+++ getlatlong.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 import sys
-from flask_jsglue import JSGlue
+import json
+#from flask_jsglue import JSGlue
 
 app = Flask(__name__)
-jsglue = JSGlue(app)
+#jsglue = JSGlue(app)
 
 @app.route('/index/', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
@@ -13,15 +14,13 @@ def index():
 @app.route("/get_lat_lng", methods=["GET", "POST"])
 def get_lat_lng():
     print('Flask!')
-    if request.method == "GET":
-        print(request.args)
-'''     print('POST!')
-        data = {}
-        data['lat'] = request.json['lat']
-        data['lng'] = request.json['lng']
-        print(str(data))
-        return jsonify(data)
-'''
+    if request.method == "POST":
+        print('It\'s POST!')
+        latitude = request.json.get('lat')
+        longitude = request.json.get('lng')
+        address = request.json.get('addr')
+        print('{}, {}, {}'.format(latitude, longitude, address))
+    return 'Post triggered'
 
 # Run Flask server
 if __name__ == '__main__':
